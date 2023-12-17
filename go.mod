8110cbcdae98d4cf30117503736232b1b50ee3b9
Om Jogani <om080jogani@gmail.com>
2023-12-17 22:39:13 +0530

    [Revamped]: Dependecies & added Broadcast Data

--- go.mod
+++ go.mod
@@ -1,8 +1,5 @@
-module github.com/omjogani/websocket
+module github.com/omjogani/websocket-go
 
 go 1.20
 
-require (
-	github.com/gorilla/websocket v1.5.1 // indirect
-	golang.org/x/net v0.17.0 // indirect
-)
+require golang.org/x/net v0.17.0
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"golang.org/x/net/websocket"
 )
@@ -15,6 +16,16 @@ type Server struct {
 func NewServer() *Server {
 	return &Server{
 		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWSOrderbook(ws *websocket.Conn) {
+	fmt.Println("New Incoming Connection from client to OrderBook Feed:", ws.RemoteAddr())
+
+	for {
+		payload := fmt.Sprintf("OrderData -> %d\n", time.Now().UnixNano())
+		ws.Write([]byte(payload))
+		time.Sleep(time.Second * 2)
 	}
 }
 
@@ -38,13 +49,25 @@ func (s *Server) readLoop(ws *websocket.Conn) {
 			continue
 		}
 		msg := buf[:n]
+		s.broadcast(msg)
 		fmt.Println("Message:", string(msg))
 		ws.Write([]byte("Thank you for the Message!!"))
+	}
+}
+
+func (s *Server) broadcast(b []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("Write Error:", err)
+			}
+		}(ws)
 	}
 }
 
 func main() {
 	server := NewServer()
 	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.Handle("/orderbookfeed", websocket.Handler(server.handleWSOrderbook))
 	http.ListenAndServe(":3550", nil)
 }
